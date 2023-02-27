a95869d422e1e9d775d3abb4b5cae9105d95ed68
Hal Perriott <hal@perriott.com>
2023-02-26 19:15:39 -0500

    change year

--- Views/dbo.vAssocSeqLineItems.sql
+++ Views/dbo.vAssocSeqLineItems.sql
@@ -2,9 +2,10 @@ SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
-CREATE VIEW [dbo].[vAssocSeqLineItems]
-AS
-SELECT     OrderNumber, LineNumber, Model
-FROM         dbo.vDMPrep
-WHERE     (FiscalYear = '2013')
+CREATE   VIEW [dbo].[vAssocSeqLineItems]  
+AS  
+SELECT     OrderNumber, LineNumber, Model  
+FROM         dbo.vDMPrep  
+WHERE     (FiscalYear = '2014')  
+-- SELECT * FROM dbo.vDMPrep  
 GO
